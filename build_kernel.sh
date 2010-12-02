ee168a6f21f3b19a4ddbd0f77c8e4e1fab427bfa
geeknik <brian.carpenter@gmail.com>
2010-12-02 06:40:29 -0600

    sync localhost to github

---
+++ build_kernel.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DATE=$(date +%m%d)
+rm "$DATE"_test_*.zip
+#rm -rf ../initramfs/.git
+#rm -rf ../voodoo_initramfs/.git
+
+for CONFIG in novoodoo
+do
+	make mrproper
+	make clean
+	rm update/*.zip update/kernel_update/zImage
+
+	make ARCH=arm geeknik_"$CONFIG"_defconfig
+	make -j6 CROSS_COMPILE=../arm-2009q3/bin/arm-none-linux-gnueabi- \
+		ARCH=arm HOSTCFLAGS="-g -O2"
+
+	cp arch/arm/boot/zImage update/kernel_update/zImage
+	cd update
+	zip -r kernel_update.zip . 
+	mv kernel_update.zip ../"$DATE"_test_"$CONFIG".zip
+	cd ..
+done
