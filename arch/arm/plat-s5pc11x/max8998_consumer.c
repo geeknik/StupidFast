7bb3d279d4e00199a97c44470a0dfa3c77540ffa
geeknik <brian.carpenter@gmail.com>
2010-12-18 02:09:58 -0600

    bump up voltages again

--- arch/arm/plat-s5pc11x/max8998_consumer.c
+++ arch/arm/plat-s5pc11x/max8998_consumer.c
@@ -81,7 +81,7 @@ static const unsigned int frequency_match_1GHZ[][2] = {
         {1150000, 1300, 1100, 0},
         {800000, 1125, 1100, 1},
         {400000, 1000, 1100, 2},
-        {200000, 925, 1000, 4},
+        {200000, 900, 1000, 4},
         {100000, 900, 1000, 5},
 #else //just for dvs test
         {1000000, 1250, 1100, 0},
@@ -94,10 +94,10 @@ static const unsigned int frequency_match_1GHZ[][2] = {
 
 static const unsigned int frequency_match_800MHZ[][2] = {
 /* frequency, Mathced VDD ARM voltage , Matched VDD INT*/
-        {800000, 1100, 1100, 0},
+        {800000, 1200, 1100, 0},
         {400000, 1050, 1100, 1},
-        {200000, 925, 1100, 3},
-        {100000, 900, 1000, 4},
+        {200000, 950, 1100, 3},
+        {100000, 950, 1000, 4},
 };
 const unsigned int (*frequency_match[2])[2] = {
         frequency_match_1GHZ,
@@ -151,9 +151,9 @@ const unsigned int (*dvs_volt_table[2])[3] = {
 
 static const unsigned int dvs_arm_voltage_set[][2] = {
 	{DVSARM1, 1300},
-	{DVSARM2, 1125},
-	{DVSARM3, 1000},
-	{DVSARM4, 925},
+	{DVSARM2, 1200},
+	{DVSARM3, 1050},
+	{DVSARM4, 950},
 	{DVSINT1, 1100},
 	{DVSINT2, 1000},
 };
