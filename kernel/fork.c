6309d9f18ff57843b331ebfb6b21df01aa9dd067
geeknik <brian.carpenter@gmail.com>
2010-12-18 20:49:21 -0600

    Revert "misc kernel patches"
    This reverts commit 20d7cbf68325252ef878ddaf987a4252588dc6dc.
    
    Conflicts:
    
    	kernel/fork.c

--- kernel/fork.c
+++ kernel/fork.c
@@ -750,10 +750,35 @@ fail_nomem:
 	return retval;
 }
 
+static struct fs_struct *__copy_fs_struct(struct fs_struct *old)
+{
+	struct fs_struct *fs = kmem_cache_alloc(fs_cachep, GFP_KERNEL);
+	/* We don't need to lock fs - think why ;-) */
+	if (fs) {
+		atomic_set(&fs->count, 1);
+		rwlock_init(&fs->lock);
+		fs->umask = old->umask;
+		read_lock(&old->lock);
+		fs->root = old->root;
+		path_get(&old->root);
+		fs->pwd = old->pwd;
+		path_get(&old->pwd);
+		read_unlock(&old->lock);
+	}
+	return fs;
+}
+
+struct fs_struct *copy_fs_struct(struct fs_struct *old)
+{
+	return __copy_fs_struct(old);
+}
+
+EXPORT_SYMBOL_GPL(copy_fs_struct);
+
 static int copy_fs(unsigned long clone_flags, struct task_struct *tsk)
 {
-	struct fs_struct *fs = current->fs;
 	if (clone_flags & CLONE_FS) {
+<<<<<<< HEAD
 		/* tsk->fs is already what we want */
 		spin_lock(&fs->lock);
 		if (fs->in_exec) {
@@ -762,9 +787,12 @@ static int copy_fs(unsigned long clone_flags, struct task_struct *tsk)
 		}
 		fs->users++;
 		spin_unlock(&fs->lock);
+=======
+		atomic_inc(&current->fs->count);
+>>>>>>> parent of 20d7cbf... misc kernel patches
 		return 0;
 	}
-	tsk->fs = copy_fs_struct(fs);
+	tsk->fs = __copy_fs_struct(current->fs);
 	if (!tsk->fs)
 		return -ENOMEM;
 	return 0;
@@ -856,11 +884,24 @@ static void posix_cpu_timers_init_group(struct signal_struct *sig)
 	/* Thread group counters. */
 	thread_group_cputime_init(sig);
 
+<<<<<<< HEAD
 	cpu_limit = ACCESS_ONCE(sig->rlim[RLIMIT_CPU].rlim_cur);
 	if (cpu_limit != RLIM_INFINITY) {
 		sig->cputime_expires.prof_exp = secs_to_cputime(cpu_limit);
 		sig->cputimer.running = 1;
 	}
+=======
+	/* Expiration times and increments. */
+	sig->it_virt_expires = cputime_zero;
+	sig->it_virt_incr = cputime_zero;
+	sig->it_prof_expires = cputime_zero;
+	sig->it_prof_incr = cputime_zero;
+
+	/* Cached expiration times. */
+	sig->cputime_expires.prof_exp = cputime_zero;
+	sig->cputime_expires.virt_exp = cputime_zero;
+	sig->cputime_expires.sched_exp = 0;
+>>>>>>> parent of 20d7cbf... misc kernel patches
 
 	/* The timer lists. */
 	INIT_LIST_HEAD(&sig->cpu_timers[0]);
@@ -874,8 +915,17 @@ static int copy_signal(unsigned long clone_flags, struct task_struct *tsk)
 
 	if (clone_flags & CLONE_THREAD)
 		return 0;
+<<<<<<< HEAD
 
 	sig = kmem_cache_zalloc(signal_cachep, GFP_KERNEL);
+=======
+	}
+	sig = kmem_cache_alloc(signal_cachep, GFP_KERNEL);
+
+	if (sig)
+		posix_cpu_timers_init_group(sig);
+
+>>>>>>> parent of 20d7cbf... misc kernel patches
 	tsk->signal = sig;
 	if (!sig)
 		return -ENOMEM;
@@ -897,7 +947,11 @@ static int copy_signal(unsigned long clone_flags, struct task_struct *tsk)
 	memcpy(sig->rlim, current->signal->rlim, sizeof sig->rlim);
 	task_unlock(current->group_leader);
 
+<<<<<<< HEAD
 	posix_cpu_timers_init_group(sig);
+=======
+	acct_init_pacct(&sig->pacct);
+>>>>>>> parent of 20d7cbf... misc kernel patches
 
 	tty_audit_fork(sig);
 
@@ -1554,6 +1608,7 @@ static int unshare_fs(unsigned long unshare_flags, struct fs_struct **new_fsp)
 {
 	struct fs_struct *fs = current->fs;
 
+<<<<<<< HEAD
 	if (!(unshare_flags & CLONE_FS) || !fs)
 		return 0;
 
@@ -1564,6 +1619,14 @@ static int unshare_fs(unsigned long unshare_flags, struct fs_struct **new_fsp)
 	*new_fsp = copy_fs_struct(fs);
 	if (!*new_fsp)
 		return -ENOMEM;
+=======
+	if ((unshare_flags & CLONE_FS) &&
+	    (fs && atomic_read(&fs->count) > 1)) {
+		*new_fsp = __copy_fs_struct(current->fs);
+		if (!*new_fsp)
+			return -ENOMEM;
+	}
+>>>>>>> parent of 20d7cbf... misc kernel patches
 
 	return 0;
 }
@@ -1679,6 +1742,7 @@ SYSCALL_DEFINE1(unshare, unsigned long, unshare_flags)
 
 		if (new_fs) {
 			fs = current->fs;
+<<<<<<< HEAD
 			spin_lock(&fs->lock);
 			current->fs = new_fs;
 			if (--fs->users)
@@ -1686,6 +1750,10 @@ SYSCALL_DEFINE1(unshare, unsigned long, unshare_flags)
 			else
 				new_fs = fs;
 			spin_unlock(&fs->lock);
+=======
+			current->fs = new_fs;
+			new_fs = fs;
+>>>>>>> parent of 20d7cbf... misc kernel patches
 		}
 
 		if (new_mm) {
@@ -1724,7 +1792,7 @@ bad_unshare_cleanup_sigh:
 
 bad_unshare_cleanup_fs:
 	if (new_fs)
-		free_fs_struct(new_fs);
+		put_fs_struct(new_fs);
 
 bad_unshare_cleanup_thread:
 bad_unshare_out:
